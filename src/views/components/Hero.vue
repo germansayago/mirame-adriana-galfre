bc8d690ae32006abf54efc7ce534531e9b1c3ebb
cmblue <german.sayago@gmail.com>
2019-07-18 13:52:04 -0300

    update home

--- src/views/components/Hero.vue
+++ src/views/components/Hero.vue
@@ -15,41 +15,37 @@
         <div class="container shape-container d-flex align-items-center">
             <div class="col px-0">
                 <div class="row justify-content-center align-items-center">
-                    <div class="col-lg-7 text-center pt-lg">
+                    <div class="col-md-6 col-lg-5 pb-5 text-center text-md-left">
+                        
                         <img src="img/brand/white.png" style="width: 200px;" class="img-fluid">
-                        <p class="lead text-white mt-4 mb-5">A beautiful Design System for Bootstrap 4. It's Free and Open Source.</p>
+
+                        <p class="lead text-white">Terapia asistida por animales en niños con parálisis cerebral</p>
+                        <p class="text-white mb-5">de Adriana Miriam Galfré</p>
+                        
                         <div class="btn-wrapper">
                             <base-button tag="a"
-                                         href="https://demos.creative-tim.com/vue-argon-design-system/documentation"
+                                         href="https://www.mercadopago.com.ar/checkout/v1/redirect?preference-id=452178703-7942a0d2-9686-4ee5-9e7a-3e3ea2d838a7"
                                          class="mb-3 mb-sm-0"
-                                         type="info"
-                                         icon="fa fa-code">
-                                Components
+                                         type="warning"
+                                         icon="fa fa-shopping-bag"
+                                         target="_blank">
+                                Comprar
                             </base-button>
                             <base-button tag="a"
-                                         href="https://www.creative-tim.com/product/vue-argon-design-system"
-                                         class="mb-3 mb-sm-0"
+                                         href="mailto:adrigalfre@hotmail.com"
+                                         class="mb-3 mb-sm-0 btn-outline-white"
                                          type="white"
-                                         icon="ni ni-cloud-download-95">
-                                Download Vue
+                                         icon="fa fa-envelope">
+                                Contactame
                             </base-button>
                         </div>
                     </div>
-                </div>
-                <div class="row align-items-center justify-content-around stars-and-coded">
-                    <div class="col-sm-4">
-                        <span class="text-white alpha-7 ml-3">Star us on</span>
-                        <a href="https://github.com/creativetimofficial/argon-design-system" target="_blank" title="Support us on Github">
-                            <img src="img/brand/github-white-slim.png" style="height: 22px; margin-top: -3px">
-                        </a>
+                    <div class="col-md-6 col-lg-7">
+                        <div class="my-3 my-sm-0">
+                            <img src="img/theme/book-1.png" class="img-fluid">
+                        </div>
                     </div>
-                    <div class="col-sm-4 mt-4 mt-sm-0 text-right">
-                        <span class="text-white alpha-7">Coded by</span>
-                        <a href="https://www.creative-tim.com" target="_blank" title="Creative Tim - Premium Bootstrap Themes and Templates">
-                            <img src="img/brand/creativetim-white-slim.png" class="ml-3" style="height: 30px;">
-                        </a>
-                    </div>
-                </div>
+                </div>               
             </div>
         </div>
     </section>
